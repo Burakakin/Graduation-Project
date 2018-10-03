e3465de978f66ed1ad5377d522648ed8c3e362aa
Burak Akin <burakakin@buraks.local>
2018-10-03 18:48:09 +0300

    Initial Commit

---
+++ GraduationProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraduationProject
+//
+//  Created by Burak Akin on 3.10.2018.
+//  Copyright © 2018 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
