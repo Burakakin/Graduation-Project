f62f8bebd5d7f184a95e12a3eb9f588101625146
Burak Akin <burakakin@buraks.local>
2018-10-03 21:14:26 +0300

    Hide nabber in menu and small changes

--- GraduationProject/LeftSideMenuViewController.swift
+++ GraduationProject/LeftSideMenuViewController.swift
@@ -10,21 +10,24 @@ import UIKit
 
 class LeftSideMenuViewController: UIViewController {
 
+    
+    @IBOutlet weak var myAccountButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        myAccountButton.layer.cornerRadius = 4
+        myAccountButton.layer.borderWidth = 1
+        myAccountButton.layer.borderColor = UIColor.black.cgColor
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    */
+    
+    
 
 }
